a41e71b4a864f53a6e34e29f08058f35f68e9e43
Karolina Gustaitis <rakolina@gmail.com>
2022-06-13 12:09:33 +0300

    save progress simplified the depth checks and seems we have a working method TODO finish testing edge cases

--- test_world_depths.py
+++ test_world_depths.py
@@ -5,40 +5,53 @@ from world_depths import find_deepest_pool
 
 class Test ( TestCase ):
 
-    # world_points = [ 2, 1, 2, 3, 1, 3, 0, 1, 2, 6, 3, 5, 2, 1, 4, 9, 7, 8 ]
-
-    def test_find_deepest_pool01 ( self ):
+    def test_empty ( self ):
         points = [ ]
         expected = 0
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_find_deepest_pool02 ( self ):
-        points = [ 2, 1, -1 ]
+    def test_out_of_bounds ( self ):
+        points = [ -9, 2, 1 ]
         expected = -1
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_find_deepest_pool03 ( self ):
-        points = [ 0, 1, 2, 3, 4, 3, 2, 1, 0 ]
+    def test_single_peak ( self ):
+        points = [ 1, 20, 1 ]
         expected = 0
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_find_deepest_pool04 ( self ):
-        points = [ 6, 5, 4, 3, 4, 5, 6 ]
-        expected = 3
+    def test_single_dip ( self ):
+        points = [ 91, 2, 8 ]
+        expected = 6
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_find_deepest_pool05 ( self ):
-        points = [ 0, 2, 1, 2, 4, 1, 3, 0, 1, 2 ]
-        expected = 2
+    def test_double_peak ( self ):
+        points = [ 4, 1, 100, 2, 10 ]
+        expected = 8
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_find_deepest_pool06 ( self ):
-        points = [ 6, 3, 5, 2, 1, 4, 9, 7, 8 ]
-        expected = 4
+    def test_plateau ( self ):
+        points = [ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 ]
+        expected = 0
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
+
+    def test_double_plateau ( self ):
+        points = [ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+        expected = 0
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
+
+    def test_three_peaks ( self ):
+        points = [ 1, 12, 3, 50, 1, 29, 9 ]
+        expected = 28
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
+
+    def test_world_edge ( self ):
+        points = [ 1, 1, 1, 2, 1, 2, 2, 2 ]
--- world_depths.py
+++ world_depths.py
@@ -2,40 +2,40 @@
 
 # Class expects non-negative integers list1
 #
-# we have a 2-D world (like Terraria!)
+# In a 2-D world (like Terraria!) water pools can form between adjacent peaks
 # How deep is the deepest water pool?
+#
 # Assumptions:
 #   1. pools at world edges have depth 0
-#   2. world points are positive integers including 0
+#   2. world points are non-negative integers
 #
 # Game plan:
 #   find all local peaks
-#   safely walk all peaks
-#     find lowest point between two adjacent local maxima
-#       current pool depth is the difference
-#       between the lower of the two adjacent peaks
-#       and the deepest point between them
-#     remember the deepest pool seen so far
-#
+#   safely probe each section between adjacent peak pair
+#     walk from lower peak down to the lowest point
+#       current pool depth is the difference between lower peak and lowest point
+#       update running deepest pool
+#   return deepest pool
 
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
-DEBUG = 1
+DEBUG = 0
 
 
 # pad 0 height edges to the data set because signal.find_peaks skips edges
-def find_peak_locations ( points ):
+def locate_peaks ( points ):
     world_points = [ 0 ]
     world_points.extend ( points )
     world_points.append ( 0 )
+    # scipy.signal.find_peaks
     peak_points, _ = find_peaks ( world_points )
     return [ p - 1 for p in peak_points ]  # need list indexes
 
 
-def find_tallest_peak ( peak_points, points ):
+def find_tallest_peak ( peaks, points ):
     top = 0
-    for p in peak_points:
+    for p in peaks:
         if top < points [ p ]:
             top = points [ p ]
     return top
@@ -48,27 +48,30 @@ def find_deepest_pool ( points ):
         if p < 0:
             return -1
 
-    peak_indexes = find_peak_locations ( points )
-    if 2 > len ( peak_indexes ):
+    peaks = locate_peaks ( points )
+    if 2 > len ( peaks ):
         return 0
 
-    dip = find_tallest_peak ( peak_indexes, points )  # initialize lowest point to largest datapoint
+    if 0 != DEBUG:
+        print ( points )
+        print ( peaks )
+
     deepest_pool = 0
-    for peak_index in range ( 1, len ( peak_indexes ) ):
-
-        # walk right
-        for points_index in range ( peak_indexes [ peak_index - 1 ], peak_indexes [ peak_index ] ):
-            if points [ points_index ] >= points [ points_index + 1 ]:
-                dip = points [ points_index ]
-            else:
-                break
-
-        if points [ peak_index - 1 ] <= points [ peak_index ]:
-            if deepest_pool < points [ peak_index - 1 ] - dip:
-                deepest_pool = points [ peak_index - 1 ] - dip
+    for p in range ( 1, len ( peaks ) ) :
+        left_peak = points [ peaks [ p - 1 ] ]
+        right_peak = points [ peaks [ p ] ]
+        if left_peak <= right_peak:
+            for x in range ( peaks [ p - 1 ], peaks [ p ] ):
+                if points [ x ] >= points [ x + 1 ]:
+                    depth = left_peak - points [ x + 1 ]
+                    if depth > deepest_pool:
+                        deepest_pool = depth
         else:
-            if deepest_pool < points [ peak_index ] - dip:
-                deepest_pool = points [ peak_index ] - dip
+            for x in range ( peaks [ p ], peaks [ p - 1 ], -1 ):
+                if points [ x - 1 ] <= points [ x ]:
+                    depth = right_peak - points [ x - 1 ]
+                    if depth > deepest_pool:
+                        deepest_pool = depth
 
     if 0 != DEBUG:
         print ( "Deepest pool:", deepest_pool )
