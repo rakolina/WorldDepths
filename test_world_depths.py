5d77c6c6e100947949eb31707e322e0e1e07190f
Karolina Gustaitis <rakolina@gmail.com>
2022-06-14 09:31:35 +0300

    Couple extra tests

--- test_world_depths.py
+++ test_world_depths.py
@@ -47,11 +47,32 @@ class Test ( TestCase ):
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
+    def test_multi_plateau ( self ):
+        points = [ 40, 60, 10, 10, 10, 5, 5, 5, 1, 1, 1, 20 ]
+        expected = 19
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
+
+    def test_multi_plateau2 ( self ):
+        points = [ 40, 20, 20, 20, 6, 6, 6, 5, 5, 5, 2, 2, 2, 1, 1, 1, 7, 7, 7, 0, 0, 0, 2, 2, 2, 8, 8, 8, 10, 10, 10, 20 ]
+        expected = 7
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
+
     def test_three_peaks ( self ):
         points = [ 1, 12, 3, 50, 1, 29, 9 ]
         expected = 28
         result = find_deepest_pool ( points )
         self.assertEqual ( expected, result, points )
 
-    def test_world_edge ( self ):
-        points = [ 1, 1, 1, 2, 1, 2, 2, 2 ]
+    def test_contained_plateau ( self ):
+        points = [ 10, 10, 10, 6, 6, 6, 2, 10, 10, 10, 11 ]
+        expected = 8
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
+
+    def test_contained_plateau2 ( self ):
+        points = [ 1, 19, 2, 6, 6, 6, 20 ]
+        expected = 17
+        result = find_deepest_pool ( points )
+        self.assertEqual ( expected, result, points )
